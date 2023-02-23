868928c4ddae26a791e3b5cbc53a5a719ca76454
Amanda Ross <amikaross@gmail.com>
2023-02-23 11:31:20 -0700

    Linked list find iterative

---
+++ ruby/linked_list_find.rb
@@ -0,0 +1,28 @@
+class Node
+  attr_accessor :val, :next
+
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
+
+def linked_list_find(head, target)
+  current = head
+  while !current.nil? do 
+    return true if current.val == target
+    current = current.next
+  end
+  false
+end
+
+a = Node.new("a")
+b = Node.new("b")
+c = Node.new("c")
+d = Node.new("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+p linked_list_find(a, "e")
